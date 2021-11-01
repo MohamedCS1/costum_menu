c3708a90ecbe0069986025808f7a89cdfb3337f1
MohamedCS <you@example.com>
2021-11-01 19:44:23 +0100

    commit

---
+++ app/src/main/java/com/example/menu_challange/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.menu_challange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu ,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+}
